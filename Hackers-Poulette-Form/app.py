f264228efac73f6b0e27dd165fdb404eea7ced3a
Latteflo <sf.simion.f@gmail.com>
2024-06-15 11:53:49 +0200

    db connection

--- Hackers-Poulette-Form/app.py
+++ Hackers-Poulette-Form/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, StringField, SubmitField, RadioField
@@ -8,6 +8,7 @@ from flask_bcrypt import Bcrypt
 from dotenv import load_dotenv
 import os
 
+# Load environment variables
 load_dotenv()
 
 app = Flask(__name__)
@@ -15,8 +16,27 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 bcrypt = Bcrypt(app)
-
 db = SQLAlchemy(app)
+
+# Define the Contact model
+
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    country = db.Column(db.String(50), nullable=False)
+    gender = db.Column(db.String(1), nullable=False)
+    message = db.Column(db.Text, nullable=False)
+    subject_repair = db.Column(db.Boolean, default=False)
+    subject_order = db.Column(db.Boolean, default=False)
+    subject_other = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return f'<Contact {self.first_name} {self.last_name}>'
+
+# Define the ContactForm class
 
 
 class ContactForm(FlaskForm):
@@ -45,32 +65,35 @@ def index():
         if form.honeypot.data:  # If honeypot is filled = a bot
             return redirect(url_for('index'))
 
-        data = {
-            'first_name': form.first_name.data,
-            'last_name': form.last_name.data,
-            'email': form.email.data,
-            'country': form.country.data,
-            'gender': form.gender.data,
-            'message': form.message.data,
-            'subjects': []
-        }
+        new_contact = Contact(
+            first_name=form.first_name.data,
+            last_name=form.last_name.data,
+            email=form.email.data,
+            country=form.country.data,
+            gender=form.gender.data,
+            message=form.message.data,
+            subject_repair=form.subject_repair.data,
+            subject_order=form.subject_order.data,
+            subject_other=form.subject_other.data
+        )
 
-        if form.subject_repair.data:
-            data['subjects'].append('Repair')
-        if form.subject_order.data:
-            data['subjects'].append('Order')
-        if form.subject_other.data:
-            data['subjects'].append('Others')
+        db.session.add(new_contact)
+        db.session.commit()
 
-        return redirect(url_for('thank_you', **data))
+        return redirect(url_for('thank_you'))
 
     return render_template('index.html', form=form)
 
 
+@app.route('/submissions')
+def submissions():
+    contacts = Contact.query.all()
+    return render_template('submissions.html', contacts=contacts)
+
+
 @app.route('/thank_you')
 def thank_you():
-    data = request.args
-    return render_template('thank_you.html', data=data)
+    return render_template('thank_you.html')
 
 
 if __name__ == '__main__':
